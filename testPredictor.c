91af5945587f381a3335b8a13ea08b317f87bbf5
Ashish-Chavan <110707870+Ashish-Chavan@users.noreply.github.com>
2024-07-25 15:42:33 +0530

    Update testPredictor.c

--- testPredictor.c
+++ testPredictor.c
@@ -35,26 +35,7 @@ void quickSort(int arr[],int low,int high)
     
     int pi=partition(arr,low,high);
     
-    //Recursion Call
-    //smaller element than pivot goes left and
-    //higher element goes right
-    quickSort(arr,low,pi-1);
-    quickSort(arr,pi+1,high);
+  
   }
 }
              
- 
-int main() {
-  int arr[]={10,7,8,9,1,5};
-  int n=sizeof(arr)/sizeof(arr[0]);
-  // Function call
-  quickSort(arr,0,n-1);
-  //Print the sorted array
-  cout<<"Sorted Array\n";
-  for(int i=0;i<n;i++)
-  {
-    cout<<arr[i]<<" ";
-  }
-  return 0;
-}
-// This Code is Contributed By Diwakar Jha
